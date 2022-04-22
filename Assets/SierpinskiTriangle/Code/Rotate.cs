5e65a31a122aa2c88d78ca77ac72204c2c5be523
Kirill Smirnov <thegaldren@gmail.com>
2022-04-22 17:12:34 +0400

    Rotate.cs triangle

---
+++ Assets/SierpinskiTriangle/Code/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace SierpinskiTriangle.Code
+{
+    public class Rotate : MonoBehaviour
+    {
+        [SerializeField] private Vector3 speed = Vector3.up;
+
+        private void Update()
+        {
+            transform.Rotate(speed * Time.deltaTime);
+        }
+    }
+}
